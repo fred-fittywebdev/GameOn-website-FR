b9a2e0e8ef5b2292496de21c656c1fb8fab3b42d
fred-fittywebdev <contact@fittywebdev.fr>
2023-06-28 11:25:31 +0200

    :bug: Création de la fonction pour fermer la fenêtre modale au click sur la croix. Création du fichier .gitignore

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// Fonction permettant l'ajout de la classe 'responsive' pour gérer le menu en version mobile.
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,17 +8,38 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/*
+---------- MODAL COMPONENT ----------
+*/
+
+// Eléments du DOM et variables
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCloseEl = document.querySelector('.close')
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Fonction permettant d'ouvrir la modale.
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// Fonction permettant de fermer la modale.
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
+// Evenements pour l'ouverture de la modale, on écoute le 'click' sur le bouton "je m'inscirs" de la page d'acceuil.
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Evenements pour la fermeture de la modale au click sur la croix..
+modalCloseEl.addEventListener("click", closeModal);
+
+
+/*
+---------- END MODAL  COMPONENT ----------
+*/
+
 
 
